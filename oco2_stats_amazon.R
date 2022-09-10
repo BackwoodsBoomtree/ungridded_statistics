69ca15887d660f0499e395ee5079f5dc2be24752
Doughty <russell.doughty@ou.edu>
2022-09-10 14:53:17 -0500

    Update for OCO2 and OCO3

---
+++ oco2_stats_amazon.R
@@ -0,0 +1,196 @@
+library(terra)
+library(ncdf4)
+
+input_files <- list.files("G:/OCO2/extracted/amazon", full.names = TRUE, recursive = TRUE)
+out_name    <- "Amazon_OCO2_L2B10_2015-2021_sifd_cf"
+out_dir     <- "G:/SIF_comps/csv/oco2/"
+years       <- c(2015:2021)
+time        <- "month"
+variable    <- "Daily_SIF_757nm"
+filters     <- c("LC_PERC_2020", "cloud_flag_abp")
+threshs     <- c(90, 0)
+direct      <- c("gt", "eq")
+
+file_df <- function(input_files, year, time) {
+
+  if (time == "8-day") {
+    dates <- seq(as.Date(paste0(year,"-01-01")), as.Date(paste0((year),"-12-31")), by="days")
+    
+    # Create data frame with column for each 8-day file list
+    for (i in 1:46) {
+      
+      sub_dates <- dates[(i * 8 - 7):(i * 8)]
+      
+      sub_files <- c()
+      
+      for (j in 1:length(sub_dates)) {
+        
+        check_file <- input_files[grepl(sub_dates[j], input_files)]
+        
+        if (length(check_file) != 0) {
+          sub_files <- c(sub_files, check_file)
+        } else {
+          sub_files <- c(sub_files, NA)
+        }
+      }
+      if (i == 1) {
+        df <- cbind(sub_files)
+      } else {
+        df <- cbind(df, sub_files)
+      }
+    }
+  }
+  
+  if (time == "16-day") {
+    dates <- seq(as.Date(paste0(year,"-01-01")), as.Date(paste0((year + 1),"-12-31")), by="days")
+    
+    # Create data frame with column for each 16-day file list
+    for (i in 1:23) {
+      
+      sub_dates <- dates[(i * 16 - 15):(i * 16)]
+      
+      sub_files <- c()
+      
+      for (j in 1:length(sub_dates)) {
+        
+        check_file <- input_files[grepl(sub_dates[j], input_files)]
+        
+        if (length(check_file) != 0) {
+          sub_files <- c(sub_files, check_file)
+        } else {
+          sub_files <- c(sub_files, NA)
+        }
+      }
+      if (i == 1) {
+        df <- cbind(sub_files)
+      } else {
+        df <- cbind(df, sub_files)
+      }
+    }
+  }
+  
+  if (time == "month") {
+    dates <- seq(as.Date(paste0(year,"-01-01")), as.Date(paste0(year,"-12-31")), by="days")
+    
+    df <- data.frame(matrix(ncol = 12, nrow = 31))
+    # Create data frame with column for each month
+    for (i in 1:12){
+      if (i < 10) {
+        m <- paste0("0", i)
+      } else {
+        m <- as.character(i)
+      }
+      
+      sub_dates <- subset(dates, format.Date(dates, "%m") == m)
+      sub_files <- c()
+      
+      for (j in 1:length(sub_dates)) {
+        
+        check_file <- input_files[grepl(sub_dates[j], input_files)]
+        
+        if (length(check_file) != 0) {
+          sub_files <- c(sub_files, check_file)
+        } else {
+          sub_files <- c(sub_files, NA)
+        }
+      }
+      
+      # Force length to 31
+      if (length(sub_files) < 31) {
+        sub_files <- sub_files[1:31]
+      }
+      
+      if (i == 1) {
+        df <- cbind(sub_files)
+      } else {
+        df <- cbind(df, sub_files)
+      }
+    }
+  }
+  
+  return(df)
+}
+get_ts  <- function(df_f, variable, time, filters, threshs, direct) {
+  
+  annual_df <- data.frame(matrix(ncol = 5, nrow = 0))
+  colnames(annual_df) <- c("Mean", "SD", "SEM", "n", "filename")
+  
+  if (time == "8-day") {
+    t <- 46
+  } else if (time == "16-day") {
+    t <- 23
+  } else if (time == "month") {
+    t <- 12
+  }
+  
+  for (i in 1:t) {
+    
+    df_t <- df_f[, i]
+    df_t <- df_t[!is.na(df_t)]
+    
+    if (length(df_t) != 0) {
+      for (j in 1:length(df_t)) {
+        nc <- nc_open(df_t[j])
+        
+        # Get data for this time step
+        data <- data.frame(var = ncvar_get(nc, variable))
+        colnames(data)[1] <- variable
+        
+        # Get filters for this time step
+        if (!is.null(filters)) {
+          for (f in 1:length(filters)){
+            data <- cbind(data, f = ncvar_get(nc, filters[f]))
+            colnames(data)[(f + 1)] <- filters[f]
+          }
+        }
+        
+        nc_close(nc)
+        
+        if (j == 1){
+          ts_data <- data
+        } else {
+          ts_data <- rbind(ts_data, data)
+        }
+      }
+      
+      # filter the data
+      if (!is.null(filters)) {
+        for (f in 1:length(filters)){
+          if (direct[f] == "lt"){
+            ts_data <- ts_data[ts_data[, (f + 1)] <= threshs[f],]
+          } else if (direct[f] == "gt"){
+            ts_data <- ts_data[ts_data[, (f + 1)] >= threshs[f],]
+          } else if (direct[f] == "eq"){
+            ts_data <- ts_data[ts_data[, (f + 1)] == threshs[f],]
+          }
+        }
+      }
+      
+      annual_df[nrow(annual_df) + 1,] <- c(mean(ts_data[, 1], rm.na = TRUE),
+                                           sd(ts_data[, 1]), 
+                                           sd(ts_data[, 1]) / (sqrt(length(ts_data[, 1]))),
+                                           length(ts_data[, 1]),
+                                           basename(df_t[j]))
+      
+    } else {
+      annual_df[nrow(annual_df) + 1,] <- c(NA, NA, NA, NA, NA)
+    }
+  }
+  
+  return(annual_df)
+}
+
+# Write time series csv for each year
+for (j in 1:length(years)) {
+  
+  files   <- file_df(input_files, years[j], time)
+  ts_data <- get_ts(files, variable, time, filters, threshs, direct)
+  
+  if (j == 1) {
+    ts_out <- ts_data
+  } else {
+    ts_out <- rbind(ts_out, ts_data)
+  }
+}
+write.csv(ts_out, paste0(out_dir, out_name, ".csv"), row.names = FALSE)
+
